c6d659bc8206c067fdb07bb75b094717b7983461
sonkeuu <agatajoannawojcik@gmail.com>
2024-12-08 13:03:11 +0100

    Implemented readinf list of City structures from text file.

--- Mapa/maps.cpp
+++ Mapa/maps.cpp
@@ -46,11 +46,109 @@ int file_reader(string line, string file) {
 	return 0;
 }
 
+int file_wbw_reader(string line, string file) {
+	ifstream inputFile("example.txt");
+	if (!inputFile.is_open()) {
+		cerr << "Unable to open file" << endl;
+		return 1;
+	}
+
+	string word;
+	while (inputFile >> word) { // Read word by word
+		cout << word << endl;
+	}
+
+	inputFile.close();
+}
+
+int file_lbl_reader(string line, const string& file, vector<string>& words) {
+	ifstream inputFile(file); // Open the file
+	if (!inputFile.is_open()) {            // Check if the file is open
+		cerr << "Unable to open file" << endl;
+		return 1; // Exit with error code
+	}
+
+
+	while (getline(inputFile, line)) { // Read line by line
+		cout << line << endl;
+		stringstream ss(line);
+		string word;
+		while (ss >> word) {
+			words.push_back(word);
+			cout << word << " ";
+			cout << "koniec linii.";
+		}
+	}
+
+		inputFile.close(); // Close the file
+	}
+
+vector<City> read_lines(string fileName) 
+{
+	ifstream file(fileName);
+	string lineFromFile;
+	vector<City> result;
+	const char* del = ";";
+	
+	while (getline(file, lineFromFile))
+	{
+		// to do optymalizacji pozniej.
+		char* line = new char[lineFromFile.length() + 1];
+		strcpy_s(line, lineFromFile.length() + 1, lineFromFile.c_str());
+		
+		char* next_token1 = NULL;
+		char* token1 = NULL;
+		vector<char*> foundWords;
+
+		token1 = strtok_s(line, del, &next_token1);
+		while ((token1 != NULL)) 
+		{
+			foundWords.push_back(token1);
+			token1 = strtok_s(NULL, del, &next_token1);
+		}
+
+		if (foundWords.size() != 3) 
+		{
+			vector<City> emptyResult;
+			return emptyResult;
+		}
+
+		int kmAsInt;
+		char* km = foundWords[2];
+		try 
+		{
+			kmAsInt = stoi(km, 0, 10);
+		}
+		catch(...)
+		{
+			vector<City> emptyResult;
+			return emptyResult;
+		}
+
+		City city_item;
+		city_item.CityName = foundWords[0];
+		city_item.DestinationName = foundWords[1];
+		city_item.Distance = kmAsInt;
+		result.push_back(city_item);
+	}
+
+	return result;
+}
 
 int main() {
 	//routes_classess();
 	string line;
-	file_reader(line, "example");
+	//file_reader(line, "example");
+	//file_wbw_reader(line, "example");
+	vector<string> words;
+	//file_lbl_reader(line, "example.txt", words);
+	vector<City> mapItems = read_lines("exampleCities.txt");
+	cout << mapItems.size() << " ";
+
+	for (City city : mapItems)
+	{
+		cout << city.CityName << " ";
+	}
 
 	return 0;
 }
